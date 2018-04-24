4587ad0295c45b7f8c105e10226c66b7a1f4b539
babuparaman <babuparaman@gmail.com>
2018-04-24 12:22:26 +0530

    Initial commit

---
+++ app/src/main/java/com/ps/androidecommerce/services/MyFirebaseMessagingService.java
@@ -0,0 +1,37 @@
+package com.ps.androidecommerce.services;
+
+import android.content.Intent;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import com.ps.androidecommerce.activities.SplashScreen;
+import com.ps.androidecommerce.utils.NotificationHelper;
+
+
+/**
+ * MyFirebaseMessagingService receives notification Firebase Cloud Messaging Server
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    
+    
+    //*********** Called when the Notification is Received ********//
+    
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Intent notificationIntent = new Intent(getApplicationContext(), SplashScreen.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        NotificationHelper.showNewNotification
+                (
+                        getApplicationContext(),
+                        notificationIntent,
+                        remoteMessage.getData().get("title"),
+                        remoteMessage.getData().get("message")
+                );
+        
+    }
+    
+}
